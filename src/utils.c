fe468742044344bd39a0dac38b1708f5928a6a6c
huangsy13 <huangsy13@gmail.com>
2016-07-12 23:42:52 -0400

    update

--- src/utils.c
+++ src/utils.c
@@ -32,7 +32,7 @@ void shuffle(void *arr, size_t n, size_t size)
     }
 }
 
-void del_arg(int argc, char **argv, int index)
+void del_arg(int argc, char **argv, int index)//删除位于i处的参数，后面的整体向前移动
 {
     int i;
     for(i = index; i < argc-1; ++i) argv[i] = argv[i+1];
@@ -52,13 +52,13 @@ int find_arg(int argc, char* argv[], char *arg)
     return 0;
 }
 
-int find_int_arg(int argc, char **argv, char *arg, int def)
+int find_int_arg(int argc, char **argv, char *arg, int def)//找到后返回并删除参数
 {
     int i;
     for(i = 0; i < argc-1; ++i){
         if(!argv[i]) continue;
         if(0==strcmp(argv[i], arg)){
-            def = atoi(argv[i+1]);
+            def = atoi(argv[i+1]);//字符串转化成int
             del_arg(argc, argv, i);
             del_arg(argc, argv, i);
             break;
